293f25bf7c94e4bab5eed0f5c3f461940c257b90
emkeator <emkeator@gmail.com>
2018-01-09 17:26:57 -0700

    ran build

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-sothebys-clone',
     proxyTable: {},
 
     // Various Dev Server settings
