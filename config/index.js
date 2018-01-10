58557e893d57d24745150f124bf7a1245fa4b191
emkeator <emkeator@gmail.com>
2018-01-09 17:31:08 -0700

    changed relative asset path

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-sothebys-clone',
+    assetsPublicPath: './vue-sothebys-clone',
     proxyTable: {},
 
     // Various Dev Server settings
